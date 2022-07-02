cd417c43acc23206cc02ed88a8cd827a403ab43f
Israelian Eva <evais1@mail.ru>
2022-07-02 20:58:40 +0300

    init commit

---
+++ lib/game.rb
@@ -0,0 +1,62 @@
+class Game
+  TOTAL_ERRORS_ALLOWED = 7
+
+  def initialize(word)
+    @letters = word.upcase.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  def errors_made
+    errors.size
+  end
+
+  def letters_to_guess
+    @letters.map do |letter|
+      if @user_guesses.include?(normalize_letter(letter))
+        letter
+      end
+    end
+  end
+
+  def lost?
+    errors_allowed.zero?
+  end
+
+  def normalize_letter(letter)
+    case letter
+    when "Ё" then "Е"
+    when "Й" then "И"
+    else letter
+    end
+  end
+
+  def normalized_letters
+    @letters.map { |letter| normalize_letter(letter) }
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    if !over? && !@user_guesses.include?(normalize_letter(letter))
+      @user_guesses << normalize_letter(letter)
+    end
+  end
+
+  def won?
+    (normalized_letters - @user_guesses).empty?
+  end
+
+  def word
+    @letters.join
+  end
+end
---
+++ main.rb
@@ -0,0 +1,18 @@
+require_relative "lib/console_interface"
+require_relative "lib/game"
+
+puts "Добро пожаловать на игру «Виселица»!"
+
+word = File.readlines("#{__dir__}/data/words.txt", chomp: true).sample
+
+game = Game.new(word)
+
+console_interface = ConsoleInterface.new(game)
+
+until game.over?
+  console_interface.print_out
+  letter = console_interface.get_input
+  game.play!(letter)
+end
+
+console_interface.print_out
